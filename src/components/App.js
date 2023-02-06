ac7c473dbf8be77eb0a62894603a6618bab5b5bd
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-02-05 20:44:49 -0500

    added clear display

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useReducer } from "react";
 import "./App.css";
-import { applyNumber, changeOperation } from "../actions";
+import { applyNumber, changeOperation, clearDisplay } from "../actions";
 
 import TotalDisplay from "./TotalDisplay";
 import CalcButton from "./CalcButton";
@@ -70,7 +70,12 @@ function App() {
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"} />
+              <CalcButton
+                value={"CE"}
+                onClick={() => {
+                  dispatch(clearDisplay());
+                }}
+              />
             </div>
           </form>
         </div>
